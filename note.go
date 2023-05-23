7b8b1903af049a8009703afd462179db5bb997f9
oddstream <oddstream@googlemail.com>
2023-05-23 15:26:24 +0100

    Initial commit

--- note.go
+++ note.go
@@ -7,12 +7,13 @@ import (
 	"path"
 	"strings"
 	"time"
+	"unicode"
 )
 
 type note struct {
 	date  time.Time // will be IsZero() for undated notes
-	fname string    // the filename this note was loaded from
-	title string    // the first line of an undated note
+	fname string    // full path+filename note was loaded from (saved so it can be remove'd if note is empty)
+	title string    // the first line of an undated note, as it was when loaded (saved so file can be renamed)
 	text  string    // the text of the note, when loaded
 }
 
@@ -35,13 +36,73 @@ func (n *note) getFname() string {
 		if name == "" {
 			return ""
 		}
-		return path.Join(userConfigDir, "oddstream.games", "goldnotebook", "undated", name, ".txt")
+		return path.Join(userConfigDir, "oddstream.games", "goldnotebook", "undated", name+".txt")
 	} else {
 		return path.Join(userConfigDir, "oddstream.games", "goldnotebook",
 			fmt.Sprintf("%04d", n.date.Year()),
 			fmt.Sprintf("%02d", n.date.Month()),
-			fmt.Sprintf("%02d", n.date.Day()),
-			".txt")
+			fmt.Sprintf("%02d.txt", n.date.Day()))
+	}
+}
+
+// load undated note (using title)
+// load daily note (using time.Time)
+
+func isStringEmpty(str string) bool {
+	for _, r := range str {
+		if !unicode.IsSpace(r) {
+			return false
+		}
+	}
+	return true
+}
+
+// save note (detect name change if undated) (remove'd file for empty note)
+func (n *note) save() {
+	if isStringEmpty(n.text) {
+		os.Remove(n.fname)
+		return
+	}
+	// make sure the config dir has been created
+	userConfigDir, err := os.UserConfigDir()
+	if err != nil {
+		log.Fatal(err)
+	}
+	var dir string
+	if n.date.IsZero() {
+		dir = path.Join(userConfigDir, "oddstream.games", "goldnotebook", "undated")
+	} else {
+		dir = path.Join(userConfigDir, "oddstream.games", "goldnotebook",
+			fmt.Sprintf("%04d", n.date.Year()),
+			fmt.Sprintf("%02d", n.date.Month()))
+	}
+	err = os.MkdirAll(dir, 0755) // https://stackoverflow.com/questions/14249467/os-mkdir-and-os-mkdirall-permission-value
+	if err != nil {
+		log.Fatal(err)
+	}
+	// if path is already a directory, MkdirAll does nothing and returns nil
+
+	// now save the note text to file
+	file, err := os.Create(n.getFname()) // nb use freshly-generated title, not saved one
+	if err != nil {
+		log.Fatal(err)
+	}
+	_, err = file.Write([]byte(n.text))
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = file.Close()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// delete the old file if the note is undated and title has changed
+	if n.date.IsZero() {
+		if n.title != n.getTitle() {
+			os.Remove(n.fname)
+			n.title = n.getTitle()
+			n.fname = n.getFname()
+		}
 	}
 }
 
--- note_test.go
+++ note_test.go
@@ -6,13 +6,19 @@ import (
 )
 
 func TestNoteFname(t *testing.T) {
-	n := &note{
-		date: time.Now(),
+	n := note{
 		text: "Now is the winter of our discontent\nMade glorious summer",
 	}
-
 	fname := n.getFname()
-	println(fname)
+	if fname != "/home/gilbert/.config/oddstream.games/goldnotebook/undated/Now is the winter of our discontent.txt" {
+		t.Error("Not expecting fname to be", fname)
+	}
+
+	n.date = time.Date(2023, 05, 23, 1, 1, 1, 1, time.UTC)
+	fname = n.getFname()
+	if fname != "/home/gilbert/.config/oddstream.games/goldnotebook/2023/05/23.txt" {
+		t.Error("Not expecting fname to be", fname)
+	}
 }
 
 func TestNoteTitle(t *testing.T) {
@@ -35,4 +41,21 @@ func TestNoteTitle(t *testing.T) {
 	if title != "Now is the winter of our discontent" {
 		t.Error("Not expecting title to be", title)
 	}
+}
+
+func TestNoteSave(t *testing.T) {
+	n := &note{
+		text: "Now is the winter of our discontent\nMade glorious summer",
+	}
+	n.title = n.getTitle()
+	n.fname = n.getFname()
+	n.save()
+
+	n = &note{
+		date: time.Now(),
+		text: "Now is the winter of our discontent\nMade glorious summer",
+	}
+	n.title = n.getTitle()
+	n.fname = n.getFname()
+	n.save()
 }
